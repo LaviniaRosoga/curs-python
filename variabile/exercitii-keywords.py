f5979c98dace3a2844f193e2de7473bad2368d45
LaviniaRosoga <rlavinia27@yahoo.com>
2026-05-17 13:38:14 +0300

    Delete variabile directory

--- variabile/exercitii-keywords.py
+++
@@ -1,5 +0,0 @@
-nota_trecere = True      # Variabila primește valoarea Adevărat
-cont_sters = False         # Variabila primește valoarea Fals
-loc_parcare = None         # Variabila este declarată goală (nu are valoare)
-
-print(nota_trecere)        # Va afișa: True
--- variabile/exercitii-variabile.py
+++
@@ -1,8 +0,0 @@
-nume = "Lavinia"
-varsta = 28
-nota_proiect = 6.5
-este_pregatit = False
-print(type(nume))
-print(type(varsta))
-print(type(nota_proiect))
-print(type(este_pregatit))
